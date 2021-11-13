f1e99f94c6c8fef4340afe499bfc602ed5d67680
anamorgado23 <aldsm@iscte-iul.pt>
2021-11-13 20:18:03 +0000

    hdx

--- Projeto/src/Node.java
+++ Projeto/src/Node.java
@@ -1,15 +1,34 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class Node {
+
+public class Node implements Runnable{
 
 	private File f;
-	
+	private CloudByte[] cbv = new CloudByte[100];	
+
 	public Node(File f){
 		this.f = f;
 
 	}
 
 	public Node(){
+	try {
+		Scanner scanner = new Scanner(f);
+		while (scanner.hasNextLine()) {
+			System.out.println(scanner.nextLine());
+		}
+		scanner.close();
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	}
+
+	@Override
+	public void run() {
 
 	}
 }
