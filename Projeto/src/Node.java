bd3f5340695a0990317d8646fb91eb01aed4ff1e
VNLindelof13 <ferreiracarlosdsilva@gmail.com>
2021-11-16 21:27:33 +0000

    Push #4

--- Projeto/src/Node.java
+++ Projeto/src/Node.java
@@ -13,55 +13,52 @@ import java.io.InputStreamReader;
 
 public class Node implements Runnable{
 
-    private CloudByte[] cbv = new CloudByte[1000000];
+	private CloudByte[] cbv = new CloudByte[1000000];
 
-    public Node(File f){
-        f = new File("data.bin");
-        try {
-            byte[] fileContents= Files.readAllBytes(f.toPath());
-            for(int i=0; i<fileContents.length; i++) {
-                cbv[i] = new CloudByte(fileContents[i]);
-                System.out.println(cbv[i]);
-            }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        InjectError();
-    }
+	public Node(File f){
+		f = new File("data.bin");
+		try {
+			byte[] fileContents= Files.readAllBytes(f.toPath());
+			for(int i=0; i<fileContents.length; i++) {
+				cbv[i] = new CloudByte(fileContents[i]);
+				System.out.println(cbv[i]);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		InjectError();
+	}
 
-    public Node(){
+	public Node(){
 
-    }
+	}
 
-    public void InjectError() {
-        Scanner scanner = new Scanner(System.in);
-        int a = 0;
-        boolean xpto = true;
-        while(true) {
-            System.out.println("entrou????");
-            String line = scanner.next();
-            System.out.println(line);
-            if(line.equals("ERROR")) {
-                System.out.println(" nao d");
-                line = scanner.next();
-                try {
-                    a = Integer.parseInt(line);
-                    cbv[a].makeByteCorrupt();
-                    System.out.println(cbv[a].isParityOk());
-                } catch (NumberFormatException e) {
-                   break;
-                }
-                System.out.println("Conseguimos nao dizemos asneiras");
-            }
-        }
-        scanner.close();
-    }
+	public void InjectError() {
+		Scanner scanner = new Scanner(System.in);
+		int a = 0;
+		boolean xpto = true;
+		while(true) {
+			String line = scanner.next();
+			if(line.equals("ERROR")) {
+				if (scanner.hasNextInt()) {
+					line = scanner.next();
+					try {
+						a = Integer.parseInt(line);
+						cbv[a].makeByteCorrupt();
+					} catch (NumberFormatException e) {
+						break;
+					}
+				}
+			}
+		}
+		scanner.close();
+	}
 
 
 
-    @Override
-    public void run() {
+	@Override
+	public void run() {
 
-    }
+	}
 }
